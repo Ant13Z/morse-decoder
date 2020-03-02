13cc4de39c27d680063ec5c691530791a8729ecc
Vladimir Ermolin <vladimir.ermolin@fastdev.se>
2020-03-03 01:26:35 +0400

    init: start morse-decoder

--- src/index.js
+++ src/index.js
@@ -37,10 +37,27 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+function decode(str) {
+    let words = [];
+    for (let i = 0; i < str.length / 10; i++) {
+        let encodeWord = str.substr(i * 10, 10);
+        if (encodeWord === '**********') {
+            words.push(' ');
+            continue;
+        }
+        let morseWord = '';
+        for (let j = 0; j < 5; j++) {
+            switch (encodeWord.substr(j * 2, 2)) {
+                case '10': morseWord += '.'; break;
+                case '11': morseWord += '-'; break;
+                case '00': morseWord += '';  break;
+            }
+        }
+        words.push(MORSE_TABLE[morseWord]);
+    }
+    return words.join('');
 }
 
 module.exports = {
     decode
-}
+};
